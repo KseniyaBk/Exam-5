d3214a4667839ad969711324642d8016e3989d3f
Kseniya <tellur312@gmail.com>
2017-03-25 16:56:09 +0600

    added slick , correct css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 /**
  * Created by Ксения on 25.03.2017.
  */
+$('.slSlider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    dots: true
+});
+smoothScroll.init();
